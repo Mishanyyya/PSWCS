3979c6cfbec933434a3c9588b04d981f32f45ef6
Mishanyyya <prosekovwas@gmail.com>
2026-05-10 17:56:13 -0700

    minor-fixes dev1

--- services/users/app/core/config.py
+++ services/users/app/core/config.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -11,9 +12,19 @@ class Settings(BaseSettings):
     SQL_ECHO: bool = False
     DEFAULT_USER_ROLE: str = "user"
 
+    SEED_ADMIN_EMAIL: Optional[str] = None
+    SEED_ADMIN_PASSWORD: Optional[str] = None
+    SEED_ADMIN_FULL_NAME: Optional[str] = None
+    SEED_USER_ROLES: Optional[str] = None
+    SEED_OUTPUT_FILE: Optional[str] = None
+
+    HOST: str = "127.0.0.1"
+    PORT: int = 8001
+
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
+        extra="ignore",
     )
 
 

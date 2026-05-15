49e206bc1d7db7f91706d18544c52d23d9a40ce0
Mishanyyya <prosekovwas@gmail.com>
2026-05-15 11:43:46 -0700

    some changes

--- services/review/app/main.py
+++ services/review/app/main.py
@@ -8,7 +8,7 @@ from app.routers import moderation, reviews
 load_dotenv()
 app = FastAPI(title="Review Service", version="1.0.0", description="Сервис для управления отзывами на университеты")
 
-# Подключаем роутеры
+
 app.include_router(reviews.router)
 app.include_router(moderation.router)
 
